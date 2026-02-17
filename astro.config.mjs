4c7d08c13abecf48f8a4c45ec49083c1b1eba5ab
Mackenzie Zastrow <3211021+zastrowm@users.noreply.github.com>
2026-02-16 20:23:28 -0500

    feat: Update sidebar links to match the flatter style of strandsagent dot com (#553)
    Co-authored-by: Mackenzie Zastrow <zastrowm@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -57,7 +57,8 @@ export default defineConfig({
     components: {
       Head: './src/components/overrides/Head.astro',
       Header: './src/components/overrides/Header.astro',
-      MarkdownContent: './src/components/overrides/MarkdownContent.astro'
+      MarkdownContent: './src/components/overrides/MarkdownContent.astro',
+      Sidebar: './src/components/overrides/Sidebar.astro',
     },
   }),
   AutoImport({
